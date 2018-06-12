e05e96c08df1a2d86ea439e7a9e6992457c4f3e4
unknown <kasakadddi@gmail.com>
2018-06-12 13:46:40 +0200

    0.8.1

--- main.js
+++ main.js
@@ -54,10 +54,10 @@ if (process.platform === 'darwin') {
 //-------------------------------------------------------------------
 let win;
 
-function sendStatusToWindow(text) {
-  log.info(text);
-  win.webContents.send('message', text);
-}
+// function sendStatusToWindow(text) {
+//   log.info(text);
+//   win.webContents.send('message', text);
+// }
 function createDefaultWindow() {
   win = new BrowserWindow();
   win.webContents.openDevTools();
@@ -66,28 +66,8 @@ function createDefaultWindow() {
   });
   win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
   return win;
-}
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
-})
-autoUpdater.on('update-available', (info) => {
-  sendStatusToWindow('Update available.');
-})
-autoUpdater.on('update-not-available', (info) => {
-  sendStatusToWindow('Update not available.');
-})
-autoUpdater.on('error', (err) => {
-  sendStatusToWindow('Error in auto-updater. ' + err);
-})
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  sendStatusToWindow(log_message);
-})
-autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded');
-});
+ }
+
 app.on('ready', function() {
   // Create the Menu
   const menu = Menu.buildFromTemplate(template);
@@ -95,23 +75,58 @@ app.on('ready', function() {
 
   createDefaultWindow();
 });
-app.on('window-all-closed', () => {
-  app.quit();
+
+
+app.on('ready', function()  {
+  autoUpdater.checkForUpdatesAndNotify();
 });
 
-//
-// CHOOSE one of the following options for Auto updates
-//
 
-//-------------------------------------------------------------------
+// autoUpdater.on('checking-for-update', () => {
+//   sendStatusToWindow('Checking for update...');
+// })
+// autoUpdater.on('update-available', (info) => {
+//   sendStatusToWindow('Update available.');
+// })
+// autoUpdater.on('update-not-available', (info) => {
+//   sendStatusToWindow('Update not available.');
+// })
+// autoUpdater.on('error', (err) => {
+//   sendStatusToWindow('Error in auto-updater. ' + err);
+// })
+// autoUpdater.on('download-progress', (progressObj) => {
+//   let log_message = "Download speed: " + progressObj.bytesPerSecond;
+//   log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+//   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+//   sendStatusToWindow(log_message);
+// })
+// autoUpdater.on('update-downloaded', (info) => {
+//   sendStatusToWindow('Update downloaded');
+// });
+// app.on('ready', function() {
+//   // Create the Menu
+//   const menu = Menu.buildFromTemplate(template);
+//   Menu.setApplicationMenu(menu);
+
+//   createDefaultWindow();
+// });
+// app.on('window-all-closed', () => {
+//   app.quit();
+// });
+
+// //
+// // CHOOSE one of the following options for Auto updates
+// //
+
+// //-------------------------------------------------------------------
 // Auto updates - Option 1 - Simplest version
 //
 // This will immediately download an update, then install when the
 // app quits.
 //-------------------------------------------------------------------
-app.on('ready', function()  {
-  autoUpdater.checkForUpdatesAndNotify();
-});
+// app.on('ready', function()  {
+//   autoUpdater.checkForUpdatesAndNotify();
+// });
 
 //-------------------------------------------------------------------
 // Auto updates - Option 2 - More control
